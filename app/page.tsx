c3b48983b66e0cbec888942218a86d890de39c56
Bruce Webster <brucewebster222@gmail.com>
2024-03-25 05:33:55 -0400

    added description object foe cleaner react code

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import AboutMe from "./components/AboutMe";
 import ProjectItem from "./components/ProjectItem";
 import DepiladitaCoverImage from "../public/project-images/depiladita-cover.png";
 
+const projectDescriptions = {
+  depiladitaEsthetic:
+    "This website showcases a beauty center named Depiladita, situated in the Dominican Republic. I crafted the navigation bar and implemented various enhancements using HTML, CSS, and JavaScript",
+};
+
 export default function Home() {
   return (
     <>
@@ -10,9 +15,9 @@ export default function Home() {
         gitHubLink="https://github.com/Bruce-Webster-21/depiladita"
         websiteLink="https://bruce-webster-21.github.io/depiladita"
         projectTitle="DEPILADITA ESTHETIC"
-        projectDescription="DESCRIPTION"
+        projectDescription={projectDescriptions.depiladitaEsthetic}
         projectImg={DepiladitaCoverImage}
-        projectImgAlt="Depiladita cover Image"
+        projectImgAlt="Depiladita website cover Image"
       />
     </>
   );
