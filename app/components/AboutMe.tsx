f961712b76df2475541cb511dba73e7589fb1ba8
Bruce Webster <brucewebster222@gmail.com>
2024-03-26 11:39:50 -0400

    Changed ' to &apos;

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -30,13 +30,13 @@ const AboutMe = () => {
       </div>
       <div className="flex flex-col gap-3 text-base text-justify leading-5">
         <p>
-          Hello, I'm Bruce Webster, a Teacher and Front-end Developer fluent in
-          English, Spanish, and Italian. With a diverse background in education
-          and programming, I bring a unique blend of skills to my work. I
-          embarked on my teaching journey in 2019, where I discovered my innate
-          ability to connect with students and foster their learning. However,
-          it was in 2020 that I stumbled upon the world of programming and
-          instantly found my passion ignited.
+          Hello, I&apos;m Bruce Webster, a Teacher and Front-end Developer
+          fluent in English, Spanish, and Italian. With a diverse background in
+          education and programming, I bring a unique blend of skills to my
+          work. I embarked on my teaching journey in 2019, where I discovered my
+          innate ability to connect with students and foster their learning.
+          However, it was in 2020 that I stumbled upon the world of programming
+          and instantly found my passion ignited.
         </p>
         <p>
           Starting with Python, I delved into the intricacies of web development
@@ -46,12 +46,12 @@ const AboutMe = () => {
         </p>
         <p>
           I am passionate about bringing ideas to life through clean code,
-          user-centric design, and efficient problem-solving. If you're looking
+          user-centric design, and efficient problem-solving. If youre looking
           to enhance your online presence, I am eager to collaborate with you.
           Whether you need a modern website, an engaging e-commerce platform, or
-          a customized web application, let's work together to turn your vision
-          into a reality. Please don't hesitate to reach out to me. Together, we
-          can create something truly remarkable!
+          a customized web application, let&apos;s work together to turn your
+          vision into a reality. Please don&apos;t hesitate to reach out to me.
+          Together, we can create something truly remarkable!
         </p>
       </div>
     </div>
