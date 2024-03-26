b6fb7a3d12325cf929404f0021b538a808788932
Bruce Webster <brucewebster222@gmail.com>
2024-03-26 11:03:40 -0400

    Finished footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,23 +6,35 @@ import typescripttIcon from "@/public/icons/typescript.svg";
 
 const Footer = () => {
   return (
-    <div className="pt-10 pb-5 font-extralight flex justify-center">
-      Built with React{" "}
-      <Image className="ml-1" src={reactIcon} width={22} alt="React Icon" />,
-      Typescript{" "}
-      <Image
-        className="ml-1 mr-1"
-        src={typescripttIcon}
-        width={22}
-        alt="Typescript Icon"
-      />{" "}
-      & Tailwind CSS{" "}
-      <Image
-        className="ml-1"
-        src={tailwindIcon}
-        width={22}
-        alt="Tailwind CSS Icon"
-      />
+    <div className="flex font-extralight flex-col pt-10 pb-5">
+      <div className="flex justify-center">
+        Built with React{" "}
+        <Image className="ml-1" src={reactIcon} width={22} alt="React Icon" />,
+        Typescript{" "}
+        <Image
+          className="ml-1 mr-1"
+          src={typescripttIcon}
+          width={22}
+          alt="Typescript Icon"
+        />{" "}
+        & Tailwind CSS{" "}
+        <Image
+          className="ml-1"
+          src={tailwindIcon}
+          width={22}
+          alt="Tailwind CSS Icon"
+        />
+      </div>
+      <div className="flex justify-center gap-1">
+        <a
+          className="underline text-amber-300 font-medium"
+          href="https://github.com/Bruce-Webster-21/bruce-s-portfolio"
+          target="_blank"
+        >
+          Click Here
+        </a>
+        to browse code
+      </div>
     </div>
   );
 };
