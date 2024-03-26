6f56dabf3ea0bc770b8156c83e326bdca94671d3
Bruce Webster <brucewebster222@gmail.com>
2024-03-26 11:04:00 -0400

    Added gear animation to coming soon project

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,15 @@ export default function Home() {
             />
           );
         })}
-        <div className="w-full min-h-96 p-4 gray-bg-color border-transparent border-2 border-solid rounded-3xl transition duration-700 opacity-80 hover:opacity-100 flex justify-center items-center text-2xl font-semibold">
-          <div className="">Coming Soon...</div>
+        <div className="w-full min-h-96 p-4 gray-bg-color border-transparent border-2 border-solid rounded-3xl transition duration-700 opacity-80 hover:opacity-100 flex flex-col justify-center items-center text-2xl font-semibold">
           {/* Add animated timer */}
+          <div className="gears">
+            <div className="gears-container p-10">
+              <div className="gear-rotate"></div>
+              <div className="gear-rotate-left"></div>
+            </div>
+          </div>
+          <div className="">Coming Soon...</div>
         </div>
       </div>
       <Footer />
